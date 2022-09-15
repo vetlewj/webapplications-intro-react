109b1ffa087dc901c3ef293a7f5cb489025c2c8c
vetlewj <vetlewj@gmail.com>
2022-09-15 21:37:30 +0200

    Task 06

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import type { NextPage } from 'next';
 import MyComponent from '../components/MyComponent';
 
+const food = ['Pizza', 'Hamburger', 'Coke'];
+
 const Home: NextPage = () => {
-  return <MyComponent title='It still works' />;
+  return (
+    <>
+      <MyComponent title='It still works' />
+      <ul>
+        {food.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Home;
