fe81767a1fe8d068122b18df0340e012a7142a58
vetlewj <vetlewj@gmail.com>
2022-09-15 16:47:17 +0200

    changed starting files to https://github.com/mariuswallin/next-starter-webapp

--- src/App.js
+++
@@ -1,7 +0,0 @@
-const App = () => (
-  <div>
-    <p>Read the README.md to see the tasks</p>
-  </div>
-);
-
-export default App;
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDOM from 'react-dom';
-import './styles.scss';
-
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
