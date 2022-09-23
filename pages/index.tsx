5ec4a6cd00380d81367147d640becfc06180fe4d
vetlewj <vetlewj@gmail.com>
2022-09-23 17:20:23 +0200

    Task 10

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
 import Food from '../components/Food';
 import MyComponent from '../components/MyComponent';
 
 const food = ['Pizza', 'Hamburger', 'Coke'];
 
 const Home: NextPage = () => {
+  const [input, setInput] = useState('');
+
   return (
     <>
       <MyComponent title='It still works' />
       <Food food={food} />
       <button onClick={() => console.log('clicked')}></button>
-      <input type='text' onChange={() => console.log('change')} />
+      <input
+        type='text'
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
     </>
   );
 };
